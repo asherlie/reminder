6caf539683bba2b6bb00b452af016ef2a0c19a7f
asherlie <lieber.asher@gmail.com>
2024-02-19 15:29:35 -0500

    test.c now optionally just waits for a message

--- test.c
+++ test.c
@@ -1,8 +1,28 @@
+/*
+ * this can be used for beep by having a single struct used to send info, store it, and add it locally
+ * this struct can be used to spread a message accross local net
+*/
 #include "localnotify.h"
 
-register_ln_payload(foo, "wlp3s0", int, 0);
-int main(){
+struct string{
+    char str[10];
+    float f;
+};
 
-    broadcast_foo(3);
-    printf("%i\n", recv_foo(NULL));
+register_ln_payload(foo, "wlp3s0", int, 0);
+register_ln_payload(str, "wlp3s0", struct string, 1);
+
+int main(int a, char** b){
+    struct string s, recv;
+    if (a >= 2) {
+        strncpy(s.str, b[1], 9);
+        s.f = 94.1129;
+        broadcast_foo(1);
+        broadcast_foo(2);
+        broadcast_foo(3023);
+        broadcast_foo(1113);
+        broadcast_str(s);
+    }
+    recv = recv_str(NULL);
+    printf("\"%s\", %f\n", recv.str, recv.f);
 }
