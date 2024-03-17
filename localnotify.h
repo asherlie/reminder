adf90f493ec065cf41b7af323b97b013b2e3786a
asherlie <lieber.asher@gmail.com>
2024-03-16 22:27:54 -0400

    changes to fix remote connection

--- localnotify.h
+++ localnotify.h
@@ -4,6 +4,7 @@
  *
  * TODO: figure out how to get this working without root privileges
  */
+#include <ctype.h>
 #include <sys/socket.h>
 #include <net/if.h>
 #include <arpa/inet.h>
@@ -17,43 +18,45 @@
 #include <stddef.h>
 
 static const uint8_t bcast_addr[6] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
+static const uint8_t nuffin[6] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
 static const uint8_t tag_bytes[5] = {0xde, 0xca, 0xfd, 0xec, 0xaf};
+static const unsigned short proto = 0xdeca;
 
 static volatile int socks[2] = {-1, -1};
+static uint8_t local_addr[6] = {0};
+static volatile _Bool local_addr_set = 0;
 
 struct pkt{
     struct ethhdr ehdr;
     uint8_t tag[5];
-    char msg[5];
 }__attribute__((__packed__));
 
 static inline int get_sock(){
     int sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-    /*struct sockaddr_ll laddr = {0};*/
+    //int sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    //struct sockaddr_ll laddr = {0};
 
     if (sock == -1 || setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &(int){1}, sizeof(int)) == -1) {
         return -1;
     }
 
-    /*
-     * laddr.sll_family = AF_PACKET;
-     * laddr.sll_protocol = htons(ETH_P_ALL);
-     * laddr.sll_ifindex = if_nametoindex("eth0");
-     * laddr.sll_ifindex = if_nametoindex("wlp3s0");
-     * laddr.sll_pkttype = PACKET_BROADCAST;
-     * laddr.sll_halen = 6;
-    */
-    /*
-     * laddr.sll_addr[0] = 0x08;
-     * laddr.sll_addr[1] = 0x11;
-     * laddr.sll_addr[2] = 0x96;
-     * laddr.sll_addr[3] = 0x99;
-     * laddr.sll_addr[4] = 0x37;
-     * laddr.sll_addr[5] = 0x90;
-    */
-
-    // it works with bind() commented out. prob not needed
-    /*printf("bind: %i\n", bind(sock, (struct sockaddr*)&laddr, sizeof(struct sockaddr_ll)));*/
+/*
+ *      laddr.sll_family = AF_PACKET;
+ *      laddr.sll_protocol = htons(ETH_P_ALL);
+ *      laddr.sll_ifindex = if_nametoindex("eth0");
+ *      laddr.sll_ifindex = if_nametoindex("wlp3s0");
+ *      laddr.sll_pkttype = PACKET_BROADCAST;
+ *      laddr.sll_halen = 6;
+ *      laddr.sll_addr[0] = 0x08;
+ *      laddr.sll_addr[1] = 0x11;
+ *      laddr.sll_addr[2] = 0x96;
+ *      laddr.sll_addr[3] = 0x99;
+ *      laddr.sll_addr[4] = 0x37;
+ *      laddr.sll_addr[5] = 0x90;
+ * 
+ *     // it works with bind() commented out. prob not needed
+ *     printf("bind: %i\n", bind(sock, (struct sockaddr*)&laddr, sizeof(struct sockaddr_ll)));
+*/
     return sock;
 }
 
@@ -73,6 +76,10 @@ static inline int get_sock(){
         if (socks[0] == -1) { \
             socks[0] = get_sock(); \
         } \
+        if (!local_addr_set) { \
+            get_local_addr(iface, local_addr); \
+            local_addr_set = 1; \
+        } \
         addr.sll_family = AF_PACKET; \
         addr.sll_protocol = htons(ETH_P_ALL); \
         addr.sll_ifindex = if_nametoindex(iface); \
@@ -83,17 +90,23 @@ static inline int get_sock(){
         } \
         packet._pl = pl; \
         packet._pl_id = payload_identifier; \
+        packet._p.ehdr.h_proto = htons(ETH_P_ALL); \
+        local_addr[0] = 0x88; \
+        memcpy(packet._p.ehdr.h_source, local_addr, 6); \
+        memcpy(packet._p.ehdr.h_source, nuffin, 6); \
         memcpy(packet._p.ehdr.h_dest, bcast_addr, 6); \
         memcpy(packet._p.tag, tag_bytes, 5); \
-        packet._p.ehdr.h_proto = htons(8); \
+        /* packet._p.ehdr.h_proto = htons(8);, hmm this isn't needed */ \
         /* I believe source mac addr is auto-set */ \
         return sendto(socks[0], &packet, sizeof(struct name), 0, (struct sockaddr*)&addr, sizeof(struct sockaddr_ll)) == sizeof(struct name); \
     } \
 \
+/* TODO: remove src_addr arg if i can get it copied to header!, wait i obv can. nvm, header not returned */ \
     payload recv_##name(_Bool* success, uint8_t* src_addr){ \
         /* there's no need for packet to be on the heap because this is a macro */ \
         struct name packet = {0}; \
-        struct sockaddr_ll r_addr; \
+        struct sockaddr_ll r_addr = {0}; \
+        ssize_t pktlen; \
         socklen_t r_slen = sizeof(struct sockaddr_ll); \
         if (socks[1] == -1) { \
             socks[1] = get_sock(); \
@@ -102,7 +115,7 @@ static inline int get_sock(){
             *success = 1; \
         } \
         while (1) { \
-            if (recvfrom(socks[1], &packet, sizeof(struct name), 0, (struct sockaddr*)&r_addr, &r_slen) == -1){ \
+            if ((pktlen = recvfrom(socks[1], &packet, sizeof(struct name), 0, (struct sockaddr*)&r_addr, &r_slen)) == -1){ \
                 if (success) { \
                     *success = 0; \
                     return packet._pl; \
@@ -112,12 +125,19 @@ static inline int get_sock(){
                 packet._pl_id != payload_identifier) { \
                 continue; \
             } \
+            puts("FOUND MATCHING PACKET"); \
             if (src_addr) { \
                 /* hmm, neither of these seem to work. we can confirmed send bytes, however, 0x69 is being passed along */ \
-                memcpy(src_addr, r_addr.sll_addr+2, 6); \
+                memcpy(src_addr, packet._p.ehdr.h_source, 6); \
+                memcpy(src_addr, r_addr.sll_addr, 6); \
+                printf("lenny: %d\n", r_addr.sll_halen); \
+                p_maddr(packet._p.ehdr.h_source); \
+                puts(""); \
+                p_maddr(r_addr.sll_addr); \
+                puts(""); \
+                p_buf((uint8_t*)&packet, sizeof(struct name), 1); \
                 /* okay, all fields are properly set except for h_source, EVEN h_dest is all 0xff - this
                  * could just be a problem with same-host behavior. testing with rasp pi */ \
-                memcpy(src_addr, packet._p.ehdr.h_source, 6); \
                 printf("%d should be %d\n", r_addr.sll_family, AF_PACKET); \
                 printf("%d should be %d\n", r_addr.sll_halen, 6); \
             } \
@@ -149,4 +169,21 @@ static inline _Bool get_local_addr(char* iname, uint8_t addr[6]){
     memcpy(addr, if_mac.ifr_addr.sa_data, 6);
     close(sock);
     return 1;
+}
+static inline void p_buf(uint8_t* buf, size_t sz, _Bool showchar){
+    for (size_t i = 0; i < sz; ++i) {
+        if (showchar && isalpha(buf[i])) {
+            printf("%c  ", buf[i]);
+        }
+        else {
+            printf("%.2hX ", buf[i]);
+        }
+        if (!(i % 4)) {
+            printf(" ");
+        }
+        if (!(i % 8)) {
+            puts("");
+        }
+    }
+    puts("");
 }
